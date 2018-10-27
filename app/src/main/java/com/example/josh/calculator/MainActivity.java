2029857985577d5c78dccf8d7e38683dc54cb4ca
Josh <basicjosh@gmail.com>
2018-10-27 16:31:53 -0700

    Added functionality to all buttons except the equals button.

--- app/src/main/java/com/example/josh/calculator/MainActivity.java
+++ app/src/main/java/com/example/josh/calculator/MainActivity.java
@@ -1,13 +1,249 @@
 package com.example.josh.calculator;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String equation = "";
+
+    TextView equationText;
+
+    //region Button Definitions
+    Button zeroBtn;
+    Button oneBtn;
+    Button twoBtn;
+    Button threeBtn;
+    Button fourBtn;
+    Button fiveBtn;
+    Button sixBtn;
+    Button sevenBtn;
+    Button eightBtn;
+    Button nineBtn;
+
+    Button lparBtn;
+    Button rParBtn;
+    Button dotBtn;
+    Button clearBtn;
+    Button divideBtn;
+    Button timesBtn;
+    Button minusBtn;
+    Button plusBtn;
+    Button backBtn;
+    Button equalBtn;
+    //endregion Button Definitions
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        equationText = findViewById(R.id.eqution);
+
+        equationText.setPaintFlags(equationText.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        //region Button initialize
+        zeroBtn = findViewById(R.id.button0);
+        oneBtn = findViewById(R.id.button1);
+        twoBtn = findViewById(R.id.button2);
+        threeBtn = findViewById(R.id.button3);
+        fourBtn = findViewById(R.id.button4);
+        fiveBtn = findViewById(R.id.button5);
+        sixBtn = findViewById(R.id.button6);
+        sevenBtn = findViewById(R.id.button7);
+        eightBtn = findViewById(R.id.button8);
+        nineBtn = findViewById(R.id.button9);
+        lparBtn = findViewById(R.id.buttonLPar);
+        rParBtn = findViewById(R.id.buttonRPar);
+        dotBtn = findViewById(R.id.buttonDot);
+        clearBtn = findViewById(R.id.buttonClear);
+        divideBtn = findViewById(R.id.buttonDivide);
+        timesBtn = findViewById(R.id.buttontimes);
+        minusBtn = findViewById(R.id.buttonMinius);
+        plusBtn = findViewById(R.id.buttonPlus);
+        backBtn = findViewById(R.id.buttonBack);
+        equalBtn = findViewById(R.id.buttonEquals);
+        //endregion Button initialize
+
+        input();
+
+
     }
+
+    //Function to handel all the button inputs
+    //all the number and symbol inputs add the number or symbol to the equation string
+    // and set the equation textView to the equation string.
+    private void input(){
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                equation = "";
+                equationText.setText(equation);
+
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(equation.length() > 0) {
+                    equation = equation.substring(0, equation.length() - 1);
+                    equationText.setText(equation);
+                }
+            }
+        });
+
+        zeroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "0";
+                equationText.setText(equation);
+            }
+        });
+
+        oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "1";
+                equationText.setText(equation);
+            }
+        });
+
+        twoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "2";
+                equationText.setText(equation);
+            }
+        });
+
+        threeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "3";
+                equationText.setText(equation);
+            }
+        });
+
+        fourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "4";
+                equationText.setText(equation);
+            }
+        });
+
+        fiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "5";
+                equationText.setText(equation);
+            }
+        });
+
+        sixBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "6";
+                equationText.setText(equation);
+            }
+        });
+
+        sevenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "7";
+                equationText.setText(equation);
+            }
+        });
+
+        eightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "8";
+                equationText.setText(equation);
+            }
+        });
+
+        nineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "9";
+                equationText.setText(equation);
+            }
+        });
+
+        dotBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += ".";
+                equationText.setText(equation);
+            }
+        });
+
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "+";
+                equationText.setText(equation);
+            }
+        });
+
+        minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "-";
+                equationText.setText(equation);
+            }
+        });
+
+        timesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "X";
+                equationText.setText(equation);
+            }
+        });
+
+        divideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "/";
+                equationText.setText(equation);
+            }
+        });
+
+        lparBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += "(";
+                equationText.setText(equation);
+            }
+        });
+
+        rParBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equation += ")";
+                equationText.setText(equation);
+            }
+        });
+
+        equalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+
 }
